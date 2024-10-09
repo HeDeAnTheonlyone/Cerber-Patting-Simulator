be079923e2adec6b0d22540ba5ecd1f65df99bed
HeDeAn <tannenbaumimjuli@gmail.com>
2024-10-09 19:23:34 +0200

    .

--- Program.cs
+++ Program.cs
@@ -8,4 +8,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// DEBUG
+builder.Services.AddLogging();
+
 await builder.Build().RunAsync();
