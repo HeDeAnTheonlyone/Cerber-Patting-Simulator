cf98c65ae6c139a322d3888ca55a82fd531547e7
HeDeAn <tannenbaumimjuli@gmail.com>
2024-10-14 16:13:10 +0200

    stuff

--- wwwroot/js/db.js
+++ wwwroot/js/db.js
@@ -9,25 +9,29 @@ import {
     doc,
 } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
-
-// Initialize Firebase with the configuration details
-const app = initializeApp(firebaseConfig);
-
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
-
-// Define a function to add a new user to the Firestore database
 window.db = {
+    db: null,
+
+    init: function (configString) {
+        try {
+            let app = initializeApp(JSON.parse(configString));
+            this.db = getFirestore(app);
+            console.log("DB initalized");
+        } catch (e) {
+            console.log("Error initializing db: ", e);
+        }
+    },
+
     addData: async function (data) {
         try {
-            const docRef = await addDoc(collection(db, "counting"), data);
+            const docRef = await addDoc(collection(this.db, "counting"), data);
         } catch (e) {
             console.error("Error adding document: ", e);
         }
     },
 
     getData: async function () {
-        const querySnapshot = await getDocs(collection(db, "counting"));
+        const querySnapshot = await getDocs(collection(this.db, "counting"));
         let dataArray = querySnapshot.docs.map((doc) => ({
             id: doc.id,
             userName: doc.get("name"),
