44ac15640b70e0d6a503ca43bb89d0f237e2b711
HeDeAnTheonlyone <105547153+HeDeAnTheonlyone@users.noreply.github.com>
2024-10-15 15:23:05 +0000

    Deploying to gh-pages from @ HeDeAnTheonlyone/Cerber-Patting-Simulator@8cd2c20fea56bd22dd663323fc8a31e42c4c60d8 🚀

--- js/audioPlayer.js
+++ js/audioPlayer.js
@@ -1,36 +1,65 @@
+
+const loopStartTime = 1.874;
+const loopEndTime = 3.154;
+let audioContext = null;
+let audioBuffer = null;
+let sourceNode = null;
+
 window.audioPlayer = {
     isPlaying: false,
     isLooping: false,
 
-    play: function (audioElem) {
+    init: async function () {
+        audioContext = new (window.AudioContext || window.webkitAudioContext);
+        const response = await fetch('assets/audio/mygosh.mp3');
+        const arrayBuffer = await response.arrayBuffer();
+        audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+    },
+
+    play: function () {
         if (this.isPlaying) return;
 
-        if (!audioElem.hasTimeUpdateListener) {
-            audioElem.hasTimeUpdateListener = true;
-            
-            audioElem.addEventListener('timeupdate', () => {
-                if (this.isLooping == false) return;
+        createSourceNode();
 
-                if (audioElem.currentTime >= 3.120) {
-                    audioElem.currentTime = 1.874;
-                }
-            });
-        }
+        sourceNode.start();
+        setTimeout(loopAudio, loopEndTime * 1000);
 
-        audioElem.play()
         this.isPlaying = true;
         this.isLooping = true;
-
-        if (!audioElem.hasEndedListener) {
-            audioElem.hasEndedListener = true;
-
-            audioElem.addEventListener('ended', () => {
-                this.isPlaying = false;
-            })
-        }
     },
 
     setLoop: function (value) {
         this.isLooping = value;
     }
 };
+
+function loopAudio () {
+    if (!audioPlayer.isLooping) return;
+
+    disposeSourceNode();
+    createSourceNode();
+
+    sourceNode.start(0, loopStartTime);
+    setTimeout(loopAudio, (loopEndTime - loopStartTime) * 1000);
+
+}
+
+function createSourceNode() {
+    sourceNode = audioContext.createBufferSource();
+    sourceNode.buffer = audioBuffer;
+    sourceNode.connect(audioContext.destination);
+    sourceNode.onended = onAudioEnded;
+}
+
+function disposeSourceNode() {
+    sourceNode.stop();
+    sourceNode.disconnect();
+    sourceNode = null;
+}
+
+function onAudioEnded() {
+    if (audioPlayer.isLooping) return;
+    audioPlayer.isPlaying = false;
+    disposeSourceNode();
+    window.dotNetObjRef.invokeMethodAsync("OnAudioEnded");
+}
